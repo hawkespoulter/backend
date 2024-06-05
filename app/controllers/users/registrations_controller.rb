155b96f0adb805e0b94cc561242826d1865e8d1a
Nathan Geronimo <nathangero@yahoo.com>
2024-06-05 14:08:13 -0700

    fix registration permissions for :name

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,9 +4,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   private
 
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
   def respond_with(resource, _opts = {})
     if request.method == "POST" && resource.persisted?
-      logger.info("User signed up successfully. User ID: #{resource.id}, Email: #{resource.email}, Name: #{resource.name}, Resource: #{resource}")
+      # logger.info("User signed up successfully. User ID: #{resource.id}, Email: #{resource.email}, Name: #{resource.name}")
 
       render json: {
         status: {code: 200, message: "Signed up sucessfully."},
