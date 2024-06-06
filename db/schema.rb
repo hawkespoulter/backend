f408219d079b4cb0dc1922d8c11c1be7243d467e
Nathan Geronimo <nathangero@yahoo.com>
2024-06-06 13:06:58 -0700

    create user_lobbies table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_06_195316) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_06_200126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_06_195316) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_active", default: false, null: false
+  end
+
+  create_table "user_lobbies", force: :cascade do |t|
+    t.bigint "lobby_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lobby_id"], name: "index_user_lobbies_on_lobby_id"
+    t.index ["user_id"], name: "index_user_lobbies_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +53,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_06_195316) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_lobbies", "lobbies"
+  add_foreign_key "user_lobbies", "users"
 end
